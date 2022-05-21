04dcb498bcf5220c095ed59b6ba413c8c06e7d32
Kocherin Dmitriy <dko4erin@gmail.com>
2022-05-21 14:01:12 +0300

    Объеднил проверки в 1 класс CreateCardTest т.к. дублируется код . Удалил класс CreateCardPositiveTest. Добавил проверки на граничные значения.

--- src/test/java/CreateCardTest.java
+++ src/test/java/CreateCardTest.java
@@ -6,22 +6,30 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-public class CreateCardNegativeTest {
+public class CreateCardTest {
 
     private final String name;
     private final boolean answer;
 
-    public CreateCardNegativeTest(String name, boolean answer) {
+
+    public CreateCardTest(String name, boolean answer) {
         this.name = name;
         this.answer = answer;
+
+
     }
 
     @Parameterized.Parameters (name = "{index} => name={0}, answer={1}") // добавили аннотацию
     public static Object[][] getData() {
         return new Object[][]{
+                {"Name Name",true}, //AllСonditionsSuccessful
+                {"N 3",true}, //BoundaryValues BottomLine 3 Symbols
+                {"QW 4",true},//BoundaryValues BottomLine 4 Symbols
+                {"Q2",false},//BoundaryValues BottomLine 2 Symbols
                 {"",false}, //Empty
-                {"QW",false},//<2
-                {"QWQWWQQQQQQQQQQQQQQQ",false},//>19
+                {"QWQWWQQQQQQQQQQQ18",true},//BoundaryValues UpperBound =18
+                {"QWQWWQQQQQQQQQQQQ19",true},//BoundaryValues UpperBound =19
+                {"WQWWQQQQQQQQQQQQQQ20",false},//BoundaryValues UpperBound =20
                 {"WWQQ QQQQQQ QQQQ",false},//>2 SPACE
                 {" QWQWWQQQQQ",false},//Start with SPACE
                 {"QWQWWQQQQQ ",false},//END with SPACE
@@ -29,7 +37,7 @@ public class CreateCardNegativeTest {
         };
     }
 
-    @DisplayName("Negative tests to CardHolder") // имя теста
+    @DisplayName("Tests to CardHolder") // имя теста
     @Description("Empty, <2,>18, >2 SPACE,Start with SPACE,END with SPACE") // описание теста
     @Test
     public void CreateCardNegativeTest() {
